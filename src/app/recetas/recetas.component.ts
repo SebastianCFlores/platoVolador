261bd264e9edd76c2d08020dd2caa0f1e3ef42a2
Sebastian_Cespedes <sebastian.cespedes1@unmsm.edu.pe>
2024-02-12 17:30:12 -0500

    Conexion a la base de datos, haciendo pruebas con el componente recetas

--- src/app/recetas/recetas.component.ts
+++ src/app/recetas/recetas.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-recetas',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './recetas.component.html',
   styleUrl: './recetas.component.css'
 })
-export class RecetasComponent {
+export class RecetasComponent implements OnInit {
+  usuarios: any[] = [];
 
+  ngOnInit(): void {
+    this.obtenerUsuarios();
+  }
+
+  obtenerUsuarios(): void {
+    const apiUrl = 'http://localhost:3000/api/usuario';
+
+    fetch(apiUrl)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then(data => {
+        this.usuarios = data;
+      })
+      .catch(error => {
+        console.error('Error al obtener usuarios', error);
+      });
+  }
 }
