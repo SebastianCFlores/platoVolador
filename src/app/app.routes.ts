a8493822071b07e6b5b562c30ed696f5807e6d1e
Renato Camacho Vega <renato159123@gmail.com>
2024-02-12 12:16:02 -0500

    habilitando enlaces

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { RegistroComponent } from './registro/registro.component';
+import { AppComponent } from './app.component';
+import { CuentaComponent } from './cuenta/cuenta.component';
+import { RegistrarIngredienteComponent } from './registrar-ingrediente/registrar-ingrediente.component';
+import { RecetasComponent } from './recetas/recetas.component';
+import { NuevaRecetaComponent } from './nueva-receta/nueva-receta.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+{path : 'registro', component: RegistroComponent},
+{path: 'cuenta', component: CuentaComponent},
+{path: 'registraringrediente', component: RegistrarIngredienteComponent},
+{path: 'recetas', component: RecetasComponent},
+{path: 'nuevareceta', component: NuevaRecetaComponent}
+];
