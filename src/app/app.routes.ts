2fe3551f0e30e72164b7c9ea3d2e6b68120d1272
Sebastian_Cespedes <sebastian.cespedes1@unmsm.edu.pe>
2024-02-12 13:13:59 -0500

    Cambio de componente cuenta por componente home, agregado de link en imagen de usuario

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,9 +5,11 @@ import { CuentaComponent } from './cuenta/cuenta.component';
 import { RegistrarIngredienteComponent } from './registrar-ingrediente/registrar-ingrediente.component';
 import { RecetasComponent } from './recetas/recetas.component';
 import { NuevaRecetaComponent } from './nueva-receta/nueva-receta.component';
+import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
-{path : 'registro', component: RegistroComponent},
+{path: 'home', component: HomeComponent},
+{path: 'registro', component: RegistroComponent},
 {path: 'cuenta', component: CuentaComponent},
 {path: 'registraringrediente', component: RegistrarIngredienteComponent},
 {path: 'recetas', component: RecetasComponent},
