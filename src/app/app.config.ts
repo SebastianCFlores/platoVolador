faa1e1fa8c6ef3c9bd73756059d702905c7703f4
Renato Camacho Vega <renato159123@gmail.com>
2024-02-18 17:53:54 -0500

    agregando firestore

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,30 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideFirebaseApp, getApp, initializeApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getStorage, provideStorage} from '@angular/fire/storage';
+
+const firebaseConfig = 
+  {
+    apiKey: "AIzaSyBuC96tE6qc2n2ncqmDzQtHKz-meY9IqYc",
+    authDomain: "miprimertestapp-9b2c3.firebaseapp.com",
+    projectId: "miprimertestapp-9b2c3",
+    storageBucket: "miprimertestapp-9b2c3.appspot.com",
+    messagingSenderId: "33710573890",
+    appId: "1:33710573890:web:64ad33f06a425dea2de0db"
+  }
+
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration(), provideAnimationsAsync()]
+  providers: [provideRouter(routes), provideClientHydration(), provideAnimationsAsync(),
+  importProvidersFrom([
+    provideFirebaseApp(() => initializeApp(firebaseConfig)),
+    provideFirestore(() => getFirestore()),
+    provideStorage(() => getStorage()),
+  ])
+  ]
 };
--- src/app/nueva-receta/nueva-receta.component.ts
+++ src/app/nueva-receta/nueva-receta.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-
+//import { provideFirebaseApp, getApp, initializeApp } from '@angular/fire/app';
+//import {getFirestore, provideFirestore } from '@angular/fire/firestore';
+import {getStorage, provideStorage, Storage} from '@angular/fire/storage';
 @Component({
   selector: 'app-nueva-receta',
   standalone: true,
@@ -8,5 +10,10 @@ import { Component } from '@angular/core';
   styleUrl: './nueva-receta.component.css'
 })
 export class NuevaRecetaComponent {
+  
+
+  constructor(private storage: Storage){
+    
+  }
 
 }
